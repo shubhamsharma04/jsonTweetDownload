b46aaa4c67ccb8c88a8383ca8ad37ff156fdfb64
opensam <opensam4@gmail.com>
2017-03-19 16:35:35 -0400

    Removed unused variable

--- src/main/java/com/tweetDownload/driver/App.java
+++ src/main/java/com/tweetDownload/driver/App.java
@@ -1,16 +1,12 @@
 package com.tweetDownload.driver;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.tweetDownload.service.TwitterStreamingService;
 
 public class App {
-	
-	@Value("${tweet.language}")
-	static String[] languages;
 	 
 	final static Logger logger = Logger.getLogger(App.class);
 
