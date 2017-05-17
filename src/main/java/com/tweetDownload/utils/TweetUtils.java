63d8414564901982378dad5c5a56840a2c770bae
opensam <opensam4@gmail.com>
2017-05-17 16:53:05 -0400

    Added a looger

--- src/main/java/com/tweetDownload/utils/TweetUtils.java
+++ src/main/java/com/tweetDownload/utils/TweetUtils.java
@@ -1,6 +1,11 @@
 package com.tweetDownload.utils;
 
+import org.apache.log4j.Logger;
+
 public final class TweetUtils {
+	
+	final static Logger logger = Logger.getLogger(TweetUtils.class);
+
 	
 	private TweetUtils(){
 		
